c709eaabb7bdda9368f12ff7a65b5aa64a37362e
Cauly <Cauliflower.kan@gmail.com>
2021-09-17 07:17:54 +0800

    Create manjaro_install.sh

---
+++ manjaro_install.sh
@@ -0,0 +1,10 @@
+sudo pacman -Syy
+sudo pacman -Syu --noconfirm
+sudo pacman -S yay --noconfirm
+yay --aururl "https://aur.tuna.tsinghua.edu.cn" --save
+yay -S i3-gaps i3lock i3status vim nitrogen dmenu picom --noconfirm
+yay -S base-devel google-chrome --noconfirm
+yay -S oh-my-zsh oh-my-zsh-powerline-theme-git --noconfirm
+yay -S rofi dunst polybar--noconfirm
+yay -S otf-apple-sf-pro --noconfirm
+yay -S vscode --noconfirm
